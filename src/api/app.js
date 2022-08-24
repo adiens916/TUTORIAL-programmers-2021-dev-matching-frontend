08ce17ff83079021c8ea40903511c258be85d06d
Inho <adiens916@naver.com>
2022-08-24 10:56:11 +0900

    refactor: Detach template from application API

---
+++ src/api/app.js
@@ -0,0 +1,17 @@
+import { get } from ".";
+
+const host = "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
+const imageHost =
+  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
+
+export async function getNodes(nodeId) {
+  try {
+    return await get(host, `/${nodeId}`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export function getImage(path) {
+  return `${imageHost}${path}`;
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,10 @@
-const host = "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
-const imageHost =
-  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
-
-export async function getNodes(nodeId) {
-  try {
-    return await get(host, `/${nodeId}`);
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-export function getImage(path) {
-  return `${imageHost}${path}`;
-}
-
-async function get(host, path) {
+export async function get(host, path) {
   const url = `${host}/${path}`;
 
   return await fetchTemplate(url);
 }
 
-async function post(host, path, data, headers = {}) {
+export async function post(host, path, data, headers = {}) {
   const url = `${host}/${path}`;
   const options = {
     method: "POST",
