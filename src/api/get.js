c3bbebe7e1045b18464b5b47a098838fc495a185
Inho <adiens916@naver.com>
2022-08-24 10:52:17 +0900

    refactor: Add template for fetch

--- src/api/get.js
+++ src/api/get.js
@@ -3,25 +3,42 @@ const imageHost =
   "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
 
 export async function getNodes(nodeId) {
-  return await get(host, `/${nodeId}`).catch((error) => console.log(error));
+  try {
+    return await get(host, `/${nodeId}`);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export function getImage(path) {
   return `${imageHost}${path}`;
 }
 
-async function get(host, path, headers = {}) {
+async function get(host, path) {
+  const url = `${host}/${path}`;
+
+  return await fetchTemplate(url);
+}
+
+async function post(host, path, data, headers = {}) {
   const url = `${host}/${path}`;
   const options = {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       ...headers,
     },
+    body: JSON.stringify(data),
   };
-  const res = await fetch(url, options);
-  const data = await res.json();
-  if (res.ok) {
+
+  return await fetchTemplate(url, options);
+}
+
+async function fetchTemplate(url, options) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (response.ok) {
     return data;
   } else {
     throw Error(data);
